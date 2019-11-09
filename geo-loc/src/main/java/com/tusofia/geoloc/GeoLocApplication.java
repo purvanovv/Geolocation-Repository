58b6e8200aaa7b173406918ab4db0fdb0d5f8f51
Viktor Parvanov <victor.parvanov@nevexis.com>
2019-11-09 10:04:50 +0200

    Initialize project be skeleton

---
+++ geo-loc/src/main/java/com/tusofia/geoloc/GeoLocApplication.java
@@ -0,0 +1,13 @@
+package com.tusofia.geoloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoLocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoLocApplication.class, args);
+	}
+
+}
---
+++ geo-loc/src/test/java/com/tusofia/geoloc/GeoLocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tusofia.geoloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoLocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
