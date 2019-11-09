c220705fb76581437c3ef42a1411461e56e061d5
Viktor Parvanov <victor.parvanov@nevexis.com>
2019-11-09 17:22:10 +0200

    Refactor

--- geo-loc/src/main/java/com/tusofia/geoloc/GeolocationsEndpoint.java
+++ geo-loc/src/main/java/com/tusofia/geoloc/GeolocationsEndpoint.java
@@ -6,26 +6,31 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import com.tusofia.generated.GetRouteRequest;
+import com.tusofia.generated.GetRouteResponse;
+import com.tusofia.geoloc.model.RouteResponse;
 import com.tusofia.geoloc.service.GeolocationService;
 
 @Endpoint
 public class GeolocationsEndpoint {
-	
+
 	private static final String NAMESPACE_URI = "https://www.geolocation.com/route/directions";
- 
+
 	private GeolocationService geolocationService;
-	
-    @Autowired
-    public GeolocationsEndpoint(GeolocationService geolocationService) {
-        this.geolocationService = geolocationService;
-    }
- 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetDirectionsRequest")
-    @ResponsePayload
-    public GetDirectionsResponse getDirections(@RequestPayload GetDirectionsRequest request) {
-    	GetDirectionsResponse response = new GetDirectionsResponse();
-        response.setStudent(this.geolocationService.getRoutes(from, to, portType, ambiguities));
- 
-        return response;
-    }
+
+	@Autowired
+	public GeolocationsEndpoint(GeolocationService geolocationService) {
+		this.geolocationService = geolocationService;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetRouteRequest")
+	@ResponsePayload
+	public GetRouteResponse getDirections(@RequestPayload GetRouteRequest request) {
+		GetRouteResponse response = new GetRouteResponse();
+		RouteResponse routeResponse = this.geolocationService.getRoute(request.getFrom(), request.getTo(),
+				request.getPortType(), request.getAmbiguities());
+
+		response.setDistance(routeResponse.getRoute().getDistance());
+		return response;
+	}
 }
--- geo-loc/src/main/java/com/tusofia/geoloc/model/Route.java
+++ geo-loc/src/main/java/com/tusofia/geoloc/model/Route.java
@@ -1,27 +1,46 @@
 package com.tusofia.geoloc.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Route {
-	private boolean hasTollRoad;
+private boolean hasTollRoad;
+	
 	private boolean hasBridge;
-	private Double distance;
+	
+	private String distance;
+	
+	private String fuelUsed;
+
 	public boolean isHasTollRoad() {
 		return hasTollRoad;
 	}
+
 	public void setHasTollRoad(boolean hasTollRoad) {
 		this.hasTollRoad = hasTollRoad;
 	}
+
 	public boolean isHasBridge() {
 		return hasBridge;
 	}
+
 	public void setHasBridge(boolean hasBridge) {
 		this.hasBridge = hasBridge;
 	}
-	public Double getDistance() {
+
+	public String getDistance() {
 		return distance;
 	}
-	public void setDistance(Double distance) {
+
+	public void setDistance(String distance) {
 		this.distance = distance;
 	}
-	
-	
+
+	public String getFuelUsed() {
+		return fuelUsed;
+	}
+
+	public void setFuelUsed(String fuelUsed) {
+		this.fuelUsed = fuelUsed;
+	}
 }
